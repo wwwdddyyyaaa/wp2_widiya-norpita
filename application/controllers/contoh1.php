8adbf7a2f49fc1acf67ddee482361200e06bfda4
wwwdddyyyaaa <widyanftaa@gmail.com>
2022-10-30 16:38:46 +0700

    tugas pertemuan 1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1  extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h>";
+        echo "Nama saya Widya Norpita
+            Saya tinggal di daerah Pontianak
+            olah raga yang saya sukai adalah
+            Sepak Bola";
+    }
+}
