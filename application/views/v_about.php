14fa6f38615c3b7014414d7633ff6d4383179eff
wwwdddyyyaaa <widyanftaa@gmail.com>
2022-10-30 19:16:55 +0700

    pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1> 
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Widya</li>
+        <li>Nama Belakang : Norpita</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Jeruju Gg Tebu Ceria no 11</li>
+    </ul>
+
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tayan</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+        <li>Berenang</li>
+    </ul>
