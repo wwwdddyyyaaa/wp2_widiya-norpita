22c422c5cdd75f4ddd72d044459e1c253443187d
wwwdddyyyaaa <widyanftaa@gmail.com>
2022-11-20 17:51:16 +0700

    tugas pertemuan 10 dan revisi sebelumnya

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,6 +1,6 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
-    <!-- row ux -->
+    <!-- row ux-->
     <div class="row">
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-danger shadow h-100 py-2 bg-primary">
@@ -17,7 +17,6 @@
                 </div>
             </div>
         </div>
-
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2 bg-warning">
                 <div class="card-body">
@@ -28,7 +27,7 @@
                                 <?php
                                 $where = ['stok != 0'];
                                 $totalstok = $this->ModelBuku->total('stok', $where);
-                                echo $totalstok;        
+                                echo $totalstok;
                                 ?>
                             </div>
                         </div>
@@ -39,29 +38,6 @@
                 </div>
             </div>
         </div>
-
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2 bg-danger">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-md font-weight-bold text-white text-uppercase mb-1">Buku yang dipinjam</div>
-                            <div class="h1 mb-0 font-weight-bold text-white">
-                                <?php
-                                $where = ['pinjam != 0'];
-                                $totaldipinjam = $this->ModelBuku->total('dipinjam', $where);
-                                echo $totaldipinjam;
-                                ?>
-                            </div>
-                        </div>
-                        <div class="col-auto">
-                            <a href="<?= base_url('user'); ?>"><i class="fas fa-user-tag fa-3x text-success"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-success shadow h-100 py-2 bg-danger">
                 <div class="card-body">
@@ -83,7 +59,6 @@
                 </div>
             </div>
         </div>
-
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-warning shadow h-100 py-2 bg-success">
                 <div class="card-body">
@@ -92,34 +67,32 @@
                             <div class="text-md font-weight-bold text-white text-uppercase mb-1">Buku yang dibooking</div>
                             <div class="h1 mb-0 font-weight-bold text-white">
                                 <?php
-                                $where = ['dibooking != 0'];
+                                $where = ['dibooking !=0'];
                                 $totaldibooking = $this->ModelBuku->total('dibooking', $where);
                                 echo $totaldibooking;
                                 ?>
                             </div>
                         </div>
                         <div class="col-auto">
-                            <a href="<?= base_url('user'); ?>"><i class="fas fa-shopping-cart fa-3x text-success"></i></a>
+                            <a href="<?= base_url('user'); ?>"><i class="fas fa-shopping-cart fa-3x text-danger"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- end row ux -->
-
-    <!-- divider -->
+    <!-- end row ux-->
+    <!-- Divider -->
     <hr class="sidebar-divider">
-
-    <!-- row table -->
+    <!-- row table-->
     <div class="row">
         <div class="table-responsive table-bordered col-sm-5 ml-auto mr-auto mt-2">
             <div class="page-header">
-                <span class="fas fa-users text-primary mt-2">Data User</span>
-                <a class="text-danger" href="<?= base_url('user/data_user'); ?>"><i class="fas fa-search mt-2 float-right">Tampilkan</i></a>
+                <span class="fas fa-users text-primary mt-2 "> Data User</span>
+                <a class="text-danger" href="<?php echo base_url('user/data_user'); ?>"><i class="fas fa-search mt-2 float-right"> Tampilkan</i></a>
             </div>
             <table class="table mt-3">
-                <thead class="table mt-3">
+                <thead>
                     <tr>
                         <th>#</th>
                         <th>Nama Anggota</th>
@@ -132,25 +105,24 @@
                 <tbody>
                     <?php
                     $i = 1;
-                    foreach ($anggota as $a) :
-                    ?>
-                    <tr>
-                        <td><?= $i++; ?></td>
-                        <td><?= $a['nama']; ?></td>
-                        <td><?= $a['email']; ?></td>
-                        <td><?= $a['role_id']; ?></td>
-                        <td><?= $a['is_active']; ?></td>
-                        <td><?= $a['tanggal_input']; ?></td>
-                    </tr>
-                    <?php endforeach; ?>
+                    foreach ($anggota as $a) { ?>
+                        <tr>
+                            <td><?= $i++; ?></td>
+                            <td><?= $a['nama']; ?></td>
+                            <td><?= $a['email']; ?></td>
+                            <td><?= $a['role_id']; ?></td>
+                            <td><?= $a['is_active']; ?></td>
+                            <td><?= date('Y', $a['tanggal_input']); ?></td>
+                        </tr>
+                    <?php } ?>
                 </tbody>
             </table>
         </div>
-
         <div class="table-responsive table-bordered col-sm-5 ml-auto mr-auto mt-2">
             <div class="page-header">
-                <span class="fas fa-book text-warning mt-2">Data Buku</span>
-                <a href="<?= base_url('user/data_user'); ?>"><i class="fas fa-search text-primary mt-2 float-right">Tampilkan</i></a>
+                <span class="fas fa-book text-warning mt-2"> Data Buku</span>
+                <a href="<?= base_url('buku'); ?>"><i class="fas fa-search text-primary mt-2 float-right">
+                        Tampilkan</i></a>
             </div>
             <div class="table-responsive">
                 <table class="table mt-3" id="table-datatable">
@@ -168,28 +140,24 @@
                     <tbody>
                         <?php
                         $i = 1;
-                        foreach ($buku as $b) :
-                        ?>
-                        <tr>
-                            <td><?= $i++; ?></td>
-                            <td><?= $b['judul_buku']; ?></td>
-                            <td><?= $b['pengarang']; ?></td>
-                            <td><?= $b['penerbit']; ?></td>
-                            <td><?= $b['tahun_terbit']; ?></td>
-                            <td><?= $b['isbn']; ?></td>
-                            <td><?= $b['stok']; ?></td>
-                        </tr>
-                        <?php endforeach; ?>
+                        foreach ($buku as $b) { ?>
+                            <tr>
+                                <td><?= $i++; ?></td>
+                                <td><?= $b['judul_buku']; ?></td>
+                                <td><?= $b['pengarang']; ?></td>
+                                <td><?= $b['penerbit']; ?></td>
+                                <td><?= $b['tahun_terbit']; ?></td>
+                                <td><?= $b['isbn']; ?></td>
+                                <td><?= $b['stok']; ?></td>
+                            </tr>
+                        <?php } ?>
                     </tbody>
                 </table>
-            </div>q
+            </div>
         </div>
-        
     </div>
-    <!-- end of row table -->
-
+    <!-- end of row table-->
 </div>
 <!-- /.container-fluid -->
-
 </div>
-<!-- end of main content -->
+<!-- End of Main Content -->
